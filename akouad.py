a3e0d4f70f84c652353dbd2633e2ca6c7d516f9f
akouad <41898371+akouad@users.noreply.github.com>
2018-07-30 19:25:34 +0200

    first commit (#8)
    * first commit
    
    * Update akouad.py
    
    * afficher le nom de grégoire

---
+++ akouad.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul 30 13:45:15 2018
+
+@author: daido
+"""
+
+print('Amal Kouadri')
+<<<<<<< HEAD
+print('Grégoire Marin')
+=======
+print('Grégoire Marin')
+>>>>>>> 36e355c7b1341a04985e744e8b1dcd8921285d96
